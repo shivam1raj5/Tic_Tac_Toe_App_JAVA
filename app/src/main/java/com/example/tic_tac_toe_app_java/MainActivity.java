2776ea3bfaa3d767d7ebb90cb3f7ab482698005c
Shivam Raj <shivam1raj5@gmail.com>
2023-08-22 01:44:04 +0530

    Layout Created and variable decleared

--- app/src/main/java/com/example/tic_tac_toe_app_java/MainActivity.java
+++ app/src/main/java/com/example/tic_tac_toe_app_java/MainActivity.java
@@ -3,12 +3,32 @@ package com.example.tic_tac_toe_app_java;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
-
+Button btn1, btn2, btn3, btn4, btn5, btn6, btn7, btn8, btn9;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        init();
+
+    }
+    private void init(){
+        btn1 = findViewById(R.id.btn1);
+        btn2 = findViewById(R.id.btn2);
+        btn3 = findViewById(R.id.btn3);
+        btn4 = findViewById(R.id.btn4);
+        btn5 = findViewById(R.id.btn5);
+        btn6 = findViewById(R.id.btn6);
+        btn7 = findViewById(R.id.btn7);
+        btn8 = findViewById(R.id.btn8);
+        btn9 = findViewById(R.id.btn9);
+    }
+
+    public void check(View view){
+
     }
 }
