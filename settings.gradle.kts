6294c7322d70a4cd32059d5bc77bec5f099349dc
Shivam Raj <shivam1raj5@gmail.com>
2023-08-21 19:00:48 +0530

    First Commit

---
+++ settings.gradle.kts
@@ -0,0 +1,17 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "Tic_Tac_Toe_App_JAVA"
+include(":app")
